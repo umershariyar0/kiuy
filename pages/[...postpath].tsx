ce6d92e244145f46bd0127cac3cdf5a5401fc411
Irfan2500866 <134538123+Irfan2500866@users.noreply.github.com>
2023-07-31 12:42:32 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/moBDY` 
+					`https://shorturl.at/jmnE9` 
 				}`,
 			},
 		};
