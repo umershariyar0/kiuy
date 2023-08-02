5a7fa1700a86aa9ee380cd095db4d9499f00473a
umershariyar0 <141007491+umershariyar0@users.noreply.github.com>
2023-08-02 16:45:31 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/szCF2` 
+					`https://shorturl.at/ouO15` 
 				}`,
 			},
 		};
